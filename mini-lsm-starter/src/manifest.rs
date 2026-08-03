c65b089f318986770a42b328be669499c1783b93
beshubh <be.shubh42@gmail.com>
2026-08-03 16:30:28 +0530

    add checksum to manifesh, add and recovery

--- mini-lsm-starter/src/manifest.rs
+++ mini-lsm-starter/src/manifest.rs
@@ -15,12 +15,12 @@
 #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
-use std::io::Read;
+use std::io::{self, Read, Seek};
 use std::path::Path;
 use std::sync::Arc;
 use std::{fs::File, io::Write};
 
-use anyhow::Result;
+use anyhow::{Context, Result, bail};
 use parking_lot::{Mutex, MutexGuard};
 use serde::{Deserialize, Serialize};
 
@@ -49,26 +49,43 @@ impl Manifest {
     }
 
     pub fn recover(path: impl AsRef<Path>) -> Result<(Self, Vec<ManifestRecord>)> {
-        let f = std::fs::OpenOptions::new()
+        let mut f = std::fs::OpenOptions::new()
             .read(true)
             .append(true)
             .open(path)?;
+        f.seek(std::io::SeekFrom::Start(0))?;
+        let mut records = vec![];
+        loop {
+            let mut length_bytes = [0u8; 4];
+            match f.read_exact(&mut length_bytes) {
+                Ok(()) => {}
+                Err(err) if err.kind() == io::ErrorKind::UnexpectedEof => {
+                    // clean EOF if no header bytes were present.
+                    // Note: read_exact alone cannot distinguish:
+                    // - zero bytes read
+                    // - partially written 4-byte header
+                    break;
+                }
+                Err(err) => return Err(err.into()),
+            }
+            let length = u32::from_be_bytes(length_bytes) as usize;
+            let mut encoded_record = vec![0u8; length];
+            f.read_exact(&mut encoded_record)
+                .context("reading manifest file for the record")?;
+            let mut checksum_bytes = [0u8; 4];
+            f.read_exact(&mut checksum_bytes)
+                .context("reading manfiest for checksum")?;
+            let expected = u32::from_be_bytes(checksum_bytes);
+            let actual = crc32fast::hash(&encoded_record);
+            if actual != expected {
+                bail!("manifest checksum mismatch: expected {expected}, got {actual}")
+            }
+            let record = serde_json::from_slice::<ManifestRecord>(&encoded_record)?;
+            records.push(record);
+        }
         let manifest = Manifest {
             file: Arc::new(Mutex::new(f)),
         };
-        let mut records = vec![];
-        let mut buf: Vec<u8> = vec![];
-        manifest.file.lock().read_to_end(&mut buf)?;
-        let stream = serde_json::Deserializer::from_slice(&buf).into_iter::<ManifestRecord>();
-        for res in stream {
-            match res {
-                Ok(record) => {
-                    records.push(record);
-                }
-                Err(e) => return Err(e.into()),
-            }
-        }
-
         Ok((manifest, records))
     }
 
@@ -85,7 +102,11 @@ impl Manifest {
         {
             let file = Arc::clone(&self.file);
             let mut guard = file.lock();
+            let length = encoded_record.len() as u32;
+            let checksum = crc32fast::hash(&encoded_record);
+            guard.write_all(&length.to_be_bytes())?;
             guard.write_all(&encoded_record)?;
+            guard.write_all(&checksum.to_be_bytes())?;
             guard.sync_all()?;
         }
         Ok(())
